1eb4c27ebc6654fc69255ff4fb54975c6dcb38c8
Shota Wake <shotawake@ShotaMacBook-Air.local>
2025-12-15 18:50:05 +0900

    update

--- src/providers/SettingsProvider.js
+++ src/providers/SettingsProvider.js
@@ -1,79 +1,55 @@
 const vscode = require('vscode');
-const { SectionItem, CheckboxControlItem, SliderControlItem, ColorControlItem, ButtonControlItem } = require('../utils/TreeItems');
+const { CheckboxControlItem, SliderControlItem, ColorControlItem } = require('../utils/TreeItems');
 const { ConfigurationManager } = require('../utils/ConfigurationManager');
 const { SLIDER_RANGES } = require('../constants');
 
-// フィルタ設定
-const FILTER_SECTIONS = [
-    ['検索', [
-        ['search', '検索', 'search']
-    ]],
-    ['ノードタイプ', [
-        ['checkbox', 'Class', 'showClass'],
-        ['checkbox', 'AbstractClass', 'showAbstractClass'],
-        ['checkbox', 'Interface', 'showInterface'],
-        ['checkbox', 'Unknown', 'showUnknown']
-    ]],
-    ['エッジタイプ', [
-        ['checkbox', 'ObjectCreate', 'showObjectCreate'],
-        ['checkbox', 'Extends', 'showExtends'],
-        ['checkbox', 'Implements', 'showImplements'],
-        ['checkbox', 'TypeUse', 'showTypeUse'],
-        ['checkbox', 'MethodCall', 'showMethodCall']
-    ]]
+const FILTER_ITEMS = [
+    ['search', '検索', 'search'],
+    ['checkbox', 'Node: Class', 'showClass'],
+    ['checkbox', 'Node: AbstractClass', 'showAbstractClass'],
+    ['checkbox', 'Node: Interface', 'showInterface'],
+    ['checkbox', 'Node: Unknown', 'showUnknown'],
+    ['checkbox', 'Link: ObjectCreate', 'showObjectCreate'],
+    ['checkbox', 'Link: Extends', 'showExtends'],
+    ['checkbox', 'Link: Implements', 'showImplements'],
+    ['checkbox', 'Link: TypeUse', 'showTypeUse'],
+    ['checkbox', 'Link: MethodCall', 'showMethodCall']
 ];
 
-// 表示設定
-const APPEARANCE_SECTIONS = [
-    ['モード', [
-        ['button', '2D/3D切り替え', 'forceGraphViewer.toggle3DMode']
-    ]],
-    ['表示設定', [
-        ['checkbox', 'スタックトレース', 'showStackTrace'],
-        ['checkbox', '名前を表示', 'showNames'],
-        ['checkbox', '短縮名を表示', 'shortNames'],
-        ['checkbox', '孤立ノードを非表示', 'hideIsolatedNodes'],
-        ['checkbox', 'ノードサイズをLOCで決定', 'nodeSizeByLoc']
-    ]],
-    ['スライス設定', [
-        ['checkbox', '順方向スライス', 'enableForwardSlice'],
-        ['checkbox', '逆方向スライス', 'enableBackwardSlice'],
-        ['slider', 'スライス深度', 'sliceDepth', SLIDER_RANGES.sliceDepth]
-    ]]
+const APPEARANCE_ITEMS = [
+    ['checkbox', '2D表示', 'mode2D'],
+    ['checkbox', '行数反映', 'nodeSizeByLoc'],
+    ['checkbox', 'スタックトレース', 'showStackTrace'],
+    ['checkbox', '名前表示', 'showNames'],
+    ['checkbox', '短縮名表示', 'shortNames'],
+    ['checkbox', '孤立ノード表示', 'showIsolatedNodes'],
+    ['checkbox', '順方向スライス', 'enableForwardSlice'],
+    ['checkbox', '逆方向スライス', 'enableBackwardSlice']
 ];
 
-// 詳細設定
-const DETAIL_SECTIONS = [
-   ['サイズ・透明度', [
-      ['slider', 'ノードサイズ', 'nodeSize', SLIDER_RANGES.nodeSize],
-      ['slider', 'リンク幅', 'linkWidth', SLIDER_RANGES.linkWidth],
-      ['slider', 'ノード透明度', 'nodeOpacity', SLIDER_RANGES.opacity],
-      ['slider', 'エッジ透明度', 'edgeOpacity', SLIDER_RANGES.opacity],
-      ['slider', '矢印サイズ', 'arrowSize', SLIDER_RANGES.arrowSize],
-      ['slider', 'テキストサイズ', 'textSize', SLIDER_RANGES.textSize]
-   ]],
-   ['レイアウト', [
-      ['slider', 'リンク距離', 'linkDistance', SLIDER_RANGES.linkDistance]
-   ]],
-   ['ノード色', [
-      ['color', 'Class', 'colorClass'],
-      ['color', 'AbstractClass', 'colorAbstractClass'],
-      ['color', 'Interface', 'colorInterface'],
-      ['color', 'Unknown', 'colorUnknown']
-   ]],
-   ['エッジ色', [
-      ['color', 'ObjectCreate', 'colorObjectCreate'],
-      ['color', 'Extends', 'colorExtends'],
-      ['color', 'Implements', 'colorImplements'],
-      ['color', 'TypeUse', 'colorTypeUse'],
-      ['color', 'MethodCall', 'colorMethodCall']
-   ]]
+const DETAIL_ITEMS = [
+    ['slider', 'スライス深度', 'sliceDepth', SLIDER_RANGES.sliceDepth],
+    ['slider', 'リンク距離', 'linkDistance', SLIDER_RANGES.linkDistance],
+    ['slider', 'ノードサイズ', 'nodeSize', SLIDER_RANGES.nodeSize],
+    ['slider', 'リンクサイズ', 'linkWidth', SLIDER_RANGES.linkWidth],
+    ['slider', 'テキストサイズ', 'textSize', SLIDER_RANGES.textSize],
+    ['slider', '矢印サイズ', 'arrowSize', SLIDER_RANGES.arrowSize],
+    ['slider', 'ノード透明度', 'nodeOpacity', SLIDER_RANGES.opacity],
+    ['slider', 'エッジ透明度', 'edgeOpacity', SLIDER_RANGES.opacity]
 ];
 
-/**
- * 統合された設定プロバイダー
- * - フィルタ、表示モード、詳細設定を1つのプロバイダーで管理
- */
+const COLOR_ITEMS = [
+    ['color', 'Node: Class', 'colorClass'],
+    ['color', 'Node: AbstractClass', 'colorAbstractClass'],
+    ['color', 'Node: Interface', 'colorInterface'],
+    ['color', 'Node: Unknown', 'colorUnknown'],
+    ['color', 'Link: ObjectCreate', 'colorObjectCreate'],
+    ['color', 'Link: Extends', 'colorExtends'],
+    ['color', 'Link: Implements', 'colorImplements'],
+    ['color', 'Link: TypeUse', 'colorTypeUse'],
+    ['color', 'Link: MethodCall', 'colorMethodCall']
+];
+
 class SettingsProvider {
     constructor() {
         this._onDidChangeTreeData = new vscode.EventEmitter();
@@ -84,15 +60,9 @@ class SettingsProvider {
         return ConfigurationManager.getInstance().loadControls();
     }
 
-    async _updateControls(data) {
-        if (data && typeof data === 'object' && 'key' in data && 'value' in data) {
-            await ConfigurationManager.getInstance().updateControl(data.key, data.value);
-        }
-    }
-
     async update(data) {
         if (data && data.key && 'value' in data) {
-            await this._updateControls(data);
+            await ConfigurationManager.getInstance().updateControl(data.key, data.value);
         }
         this._onDidChangeTreeData.fire();
     }
@@ -107,43 +77,40 @@ class SettingsProvider {
 
     getChildren(element) {
         if (!element) return this.getRootItems();
-        if (element.contextValue === 'section' || element.contextValue === 'controlSection') {
+        if (element.contextValue === 'section') {
             return element.children;
         }
         return [];
     }
 
     getRootItems() {
-        // すべてのセクションを統合
-        const allSections = [
-            ...FILTER_SECTIONS,
-            ...APPEARANCE_SECTIONS,
-            ...DETAIL_SECTIONS
+        return [
+            new SectionItem('フィルタ設定', FILTER_ITEMS.map(c => this.createControlItem(c))),
+            new SectionItem('表示モード', APPEARANCE_ITEMS.map(c => this.createControlItem(c))),
+            new SectionItem('詳細設定', DETAIL_ITEMS.map(c => this.createControlItem(c))),
+            new SectionItem('色設定', COLOR_ITEMS.map(c => this.createControlItem(c)))
         ];
-
-        return allSections.map(([label, ctrls]) =>
-            new SectionItem(label, ctrls.map(c => this.createControlItem(c)), 'controlSection')
-        );
     }
 
     createControlItem(c) {
         const [type, label, key] = c;
-        if (type === 'search') return new SearchControlItem(label, this.controls[key]);
-        if (type === 'checkbox') return new CheckboxControlItem(label, this.controls[key], key);
-        if (type === 'slider') {
-            const range = c[3];
-            return new SliderControlItem(label, this.controls[key], range.min, range.max, range.step, key);
-        }
-        if (type === 'color') return new ColorControlItem(label, this.controls[key], key);
-        if (type === 'button') return new ButtonControlItem(label, key);
-
+        const value = this.controls[key];
+        if (type === 'search') return new SearchControlItem(label, value);
+        if (type === 'checkbox') return new CheckboxControlItem(label, value, key);
+        if (type === 'slider') return new SliderControlItem(label, value, c[3].min, c[3].max, c[3].step, key);
+        if (type === 'color') return new ColorControlItem(label, value, key);
         throw new Error(`Unknown control type: ${type}`);
     }
 }
 
-/**
- * 検索コントロールアイテム
- */
+class SectionItem extends vscode.TreeItem {
+    constructor(label, children) {
+        super(label, vscode.TreeItemCollapsibleState.Collapsed);
+        this.contextValue = 'section';
+        this.children = children;
+    }
+}
+
 class SearchControlItem extends vscode.TreeItem {
     constructor(label, value) {
         super(label, vscode.TreeItemCollapsibleState.None);
